d432cb3d5287370f490f083aa699589a5118d5c0
bodrovis <ibodrov@campaigner.ru>
2012-12-11 20:53:25 +0400

    'page'

--- lib/irwi/helpers/wiki_pages_helper.rb
+++ lib/irwi/helpers/wiki_pages_helper.rb
@@ -65,7 +65,8 @@ module Irwi::Helpers::WikiPagesHelper
       text = "#$2#$3"
       link, anchor = if $1 then $1.split('#', 2) else $2 end
 	  exists = wiki_page_exists?(link)
-      "<a href=\"#{wiki_link link}#{ '#' + anchor if anchor}\" title=\"#{link}#{' (no such page)' unless exists}\" class=\"#{exists ? 'existing-page' : 'not-existing-page'}\">#{text}</a>"
+      "<a href=\"#{wiki_link link}#{ '#' + anchor if anchor}\" title=\"#{link}#{' (' + wt('no such page' + ')' unless exists}\"" + 
+	  " class=\"#{exists ? 'existing-page' : 'not-existing-page'}\">#{text}</a>"
     end.html_safe
   end
 
